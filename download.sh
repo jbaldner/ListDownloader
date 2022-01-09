b0daba11e933636db491f14f3ff18bf7c44b8e9d
jbaldner <jbaldner@protonmail.com>
2022-01-09 14:59:27 -0500

    added file

---
+++ download.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -f "$1" ]; then
+
+  while read p; do
+
+    filename=$(basename $p)
+    foldername="${filename%.*}"
+    fullfilepath=./files/$foldername/$filename
+    
+    mkdir -p "./files/"
+
+    if test -f "$fullfilepath"; then
+
+        echo "File $filename is already downloaded skipping"
+
+    else 
+
+        mkdir -p "./files/$foldername/"
+        wget "$p" 
+        mv $filename "./files/$foldername/"
+
+    fi
+
+  done <$1
+
+else 
+
+    echo "$1 does not exist."
+    
+fi
